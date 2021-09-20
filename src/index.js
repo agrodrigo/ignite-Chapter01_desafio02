de6f225d2cb724161fd4fc8a33623cdf8b4e2e6d
Rodrigo Pereira <rodrigo.geogis@gmail.com>
2021-09-20 01:54:07 -0400

    Desafio Concluído

--- src/index.js
+++ src/index.js
@@ -10,19 +10,69 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const user = users.find(user => user.username === username)
+
+  if(!user) {
+    return response.status(404).json({error: "Usuário não existe!"})
+  }
+
+  request.user = user;
+
+  return next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const user = request.user;
+
+  if(!user.pro){
+    if(user.todos.length === 10){
+      return response.status(403).json({ error : "ToDos limite achieved."});
+    }
+  }
+
+  return next();
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+  const { username } = request.headers;
+
+  const user = users.find(user => user.username === username);
+
+  if (!user) {
+    return response.status(404).json({error: "Usuário não encontrado!"})
+  };
+
+  if(!validate(id)) {
+    return response.status(400).json({error: "Esse id não é um uuid válido!"})
+  };
+
+  const todo = user.todos.find(todo => todo.id === id);
+
+  if(!todo) {
+    return response.status(404).json({error: "Todo não encontrado!"})
+  }
+
+  request.user = user;
+  request.todo = todo;
+
+  return next();
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+
+  const user = users.find(user => user.id === id);
+
+  if(!user) {
+    return response.status(404).json({error: "Usuário não encontrado!"})
+  }
+
+  request.user = user;
+
+  return next();
 }
 
 app.post('/users', (request, response) => {
